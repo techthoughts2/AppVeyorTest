fca29508ef8beceed3dde38f954b672e7024d0b9
Jake Morrison <tech@techthoughts.info>
2019-11-30 23:47:06 -0800

    Added missing dep

--- actions_bootstrap.ps1
+++ actions_bootstrap.ps1
@@ -8,17 +8,26 @@ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
 # List of PowerShell Modules required for the build
 $modulesToInstall = [System.Collections.ArrayList]::new()
+# https://github.com/pester/Pester
 $null = $modulesToInstall.Add(([PSCustomObject]@{
             ModuleName    = 'Pester'
             ModuleVersion = '4.9.0'
         }))
+# https://github.com/nightroman/Invoke-Build
 $null = $modulesToInstall.Add(([PSCustomObject]@{
             ModuleName    = 'InvokeBuild'
             ModuleVersion = '5.5.5'
         }))
+# https://github.com/PowerShell/PSScriptAnalyzer
 $null = $modulesToInstall.Add(([PSCustomObject]@{
             ModuleName    = 'PSScriptAnalyzer'
             ModuleVersion = '1.18.3'
+        }))
+# https://github.com/PowerShell/platyPS
+# older version used due to: https://github.com/PowerShell/platyPS/issues/457
+$null = $modulesToInstall.Add(([PSCustomObject]@{
+            ModuleName    = 'platyPS'
+            ModuleVersion = '0.12.0'
         }))
 
 'Installing PowerShell Modules'
